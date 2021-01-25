895f992e473f3bf8009d940889947eb7cc82dafa
Jakob Persons <jakobep24@gmail.com>
2021-01-25 17:08:52 -0500

    character functionality and styling complete

--- front-end/index.js
+++ front-end/index.js
@@ -3,6 +3,8 @@ const characterDiv = document.querySelector('#character-container')
 const login = document.querySelector('#login-form')
 const navBar = document.querySelector('#nav-bar')
 const characterDisplay = document.querySelector('#character-display')
+const charLi = document.querySelector('#create-character')
+const charForm = document.querySelector('#character-form')
 let charId 
 let allUsers
 let loginName
@@ -47,6 +49,8 @@ function getCharacter(id){
 // event listeners
 login.addEventListener('submit', handleLogin)
 characterDiv.addEventListener('click', handleCharacterSelect)
+charLi.addEventListener('click', displayCharForm)
+charForm.addEventListener('submit', createCharacter)
 
 //Event Handlers 
 function handleLogin(event){
@@ -63,6 +67,39 @@ function handleCharacterSelect(event){
     }  
 }
 
+function displayCharForm(event) {
+    if(charForm.style.display === 'none') {
+        charForm.style.display = "block"
+    } else {
+        charForm.style.display = "none"
+    }
+}
+
+function createCharacter(event){
+    event.preventDefault()
+    data = {
+        user_id: currentUser.id,
+        name: event.target.name.value,
+        image_url: event.target.image.value,
+        hp: 100,
+        ap: 50,
+        dp: 50
+    }
+    console.log(data)
+
+    fetch("http://localhost:3000/characters", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(char => {
+        characterDiv.innerHTML += 
+            `<p data-id="${char.id}">${char.name}</p>`
+    })
+    event.target.reset()
+    charForm.style.display = "none"
+}
 //helper functions
 function checkUser(allUsers){
     size = allUsers.length
@@ -93,10 +130,10 @@ function renderCharacters(allChars) {
     allChars.forEach(char => {
         if (currentUser.id === char.user_id) {
             characterDiv.innerHTML += `<p data-id="${char.id}">${char.name}</p>`
-            
         }
     })
 }
+
 
 function renderCharacter(char) {
     console.log(characterDisplay)
@@ -107,7 +144,7 @@ function renderCharacter(char) {
         <div class="flip-card">
             <div class="flip-card-inner">
                 <div class="flip-card-front">
-                    <img src="${char.image_url}" alt="${char.name}" style="width:300px;height:300px;">
+                    <img src="${char.image_url}" alt="${char.name}" style="width:350px;height:290px;">
                 </div>
                 <div class="flip-card-back">
                     <p>Health: ${char.hp}</p>
